d4e1b7172db8489f8699e3ed8ed5d574bb1db77c
Luis Felipe Perez <felipe.perez@envato.com>
2024-10-14 15:59:53 -0600

    add seeds to initialize configuration record

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+require './lib/models'
+
+Configuration.create({ canva_auth_code: '', canva_access_token: '', canva_refresh_token: '' })
---
+++ lib/models.rb
@@ -0,0 +1,5 @@
+class Configuration < ActiveRecord::Base
+end
+
+class Template < ActiveRecord::Base
+end
